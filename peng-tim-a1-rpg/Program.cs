7e7e79caee9fc71868d189a191b0f79c362703c0
yees <163901371+yees422@users.noreply.github.com>
2024-05-07 09:42:30 -0700

    add visual studio project

---
+++ peng-tim-a1-rpg/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace peng_tim_a1_rpg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
