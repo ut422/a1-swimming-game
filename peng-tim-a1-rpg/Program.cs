c51c83aa1bef7e611a02076e9590d8a17289f7ad
yees <163901371+yees422@users.noreply.github.com>
2024-05-07 22:52:54 -0700

    added extra stuff

--- peng-tim-a1-rpg/Program.cs
+++ peng-tim-a1-rpg/Program.cs
@@ -1,10 +1,61 @@
 ﻿namespace peng_tim_a1_rpg
 {
-    internal class Program
+    using System;
+
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            // what's happening
+
+            Console.WriteLine("you're at a swim meet and you're trying to get at least 2nd place.");
+
+            // choices
+
+            Console.WriteLine("how 2 swim:");
+            Console.WriteLine("1. freestyle");
+            Console.WriteLine("2. breaststroke");
+            Console.WriteLine("3. drown (why)");
+
+            string swimmingStyle = Console.ReadLine();
+
+                // 1st choice
+
+            if (swimmingStyle == "1")
+            {
+                Console.WriteLine("you choose freestyle. hey, at least you're not drowning...");
+
+
+                // 1st choice outcome
+
+                Console.WriteLine("you didn't finish below 2nd, so yay?");
+            }
+
+                // 2nd choice
+
+            else if (swimmingStyle == "2")
+            {
+                Console.WriteLine("you choose breaststroke. at least you're still not drowning...");
+
+                // 2nd choice outcome
+
+                Console.WriteLine("so close, yet so far... (you finished second last)");
+            }
+
+                // 3rd choice
+
+            else if (swimmingStyle == "3")
+            {
+                Console.WriteLine("you have drowned");
+
+                // 3rd choice outcome
+
+                Console.WriteLine(":skull emoji:");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+            }
         }
     }
 }
